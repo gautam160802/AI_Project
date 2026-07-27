b0490ea3d6ff85b3ba0150edc817ff64f522821d
gautam160802 <gk17318@gmail.com>
2026-07-27 23:16:36 +0530

    adding user routes and password hash

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,12 @@
+const { Router } = require('express');
+const authController = require("../controllers/auth.controller")
+const authRouter = Router()
+
+/*
+Shift + Alt + A
+route post /api/auth/register
+*/
+authRouter.post("/register", authController.registerUserController)
+
+
+module.exports = authRouter;

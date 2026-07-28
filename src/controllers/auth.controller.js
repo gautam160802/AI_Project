57d252ccd353f276c05c0b2bae0495fc7aa5921f
gautam160802 <gk17318@gmail.com>
2026-07-28 23:48:28 +0530

    adding the login check for user

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 
 const userModel = require("../models/user.model")
 const bcrypt = require("bcryptjs")
-
+const jwt = require("jsonwebtoken")
 
 async function registerUserController(req, res) {
        
@@ -29,6 +29,42 @@ async function registerUserController(req, res) {
         username, email, password: hash
     })
 
+    const token = jwt.sign(
+        { id: user._id, username: user.username },
+        process.env.JWT_SECRET,
+        { expiresIn: "1d" }
+    )
+
+    res.cookie("token", token)
+
+    res.status(201).json({
+        message: "User registered successfully",
+        user:{
+            id: user._id,
+            username: user.username,
+            email: user.email
+        }
+    })
+}
+
+async function loginUserController(req, res) {
+    const { email, password } = req.body
+
+    const user = await userModel.findOne({ email })
+
+    if (!user) {
+        return res.status(400).json({
+            message: "Invalid email or password"
+        })
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+
+    if(!isPasswordValid) {
+        return res.status(400).json({
+            message: "Invalid email or password"
+        })
+    }
 }
 
 module.exports = {

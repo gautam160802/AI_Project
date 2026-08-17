7d6104ee305c7859311a4616f1c14b2151a36519
gautam160802 <gk17318@gmail.com>
2026-08-17 07:52:21 +0530

    auth is completed

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,8 @@
 const { Router } = require('express');
 const authController = require("../controllers/auth.controller")
 const authRouter = Router()
+const authMiddleware = require("../middlewares/auth.middleware")
+
 
 /*
 Shift + Alt + A
@@ -12,5 +14,6 @@ authRouter.post("/login", authController.loginUserController)
 
 authRouter.get("/logout", authController.logoutUserController)
 
+authRouter.get("/get-me", authMiddleware.authUser, authController.getMeController)
 
 module.exports = authRouter;

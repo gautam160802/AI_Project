0f4f9340feb668107c37777f7cd897f4bc5d73c1
gautam160802 <gk17318@gmail.com>
2026-08-17 20:18:06 +0530

    add styling in the login form

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import "../auth.form.scss"
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <main>
+        <div className="form-container">
+            <h1>Login</h1>
+
+            <form>
+                <div className="input-group">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id="email" placeholder='Enter email address' />
+                </div>
+                <div className="input-group">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" placeholder='Enter password' />
+                </div>
+                <button className='button primary-button'>Login</button>
+            </form>
+        </div>
+    </main>
   )
 }
 

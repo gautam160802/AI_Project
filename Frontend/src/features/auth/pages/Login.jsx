493f8431b8922691fe5e12ada547102a709bcfb1
gautam160802 <gk17318@gmail.com>
2026-08-19 00:08:48 +0530

    Add Axios dependency, enhance registration and login forms with navigation links, and update styles

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "../auth.form.scss"
+import { Link } from 'react-router'
 
 const Login = () => {
 
@@ -22,6 +23,8 @@ const Login = () => {
                 </div>
                 <button className='button primary-button'>Login</button>
             </form>
+
+            <p>Don't have an account? <Link to={"/register"}>Register</Link></p>
         </div>
     </main>
   )
